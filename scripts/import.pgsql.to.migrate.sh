80c648e4acd11bf4a741f046e0a2724f52fc44b2
root <root@postgres-reinstall>
2018-04-28 02:25:58 +0000

    Import script WIP

--- scripts/import.pgsql.to.migrate.sh
+++ scripts/import.pgsql.to.migrate.sh
@@ -1,6 +1,5 @@
 echo 'DROP DATABASE treetracker_migrate' | sudo -u postgres psql
 echo 'CREATE DATABASE treetracker_migrate' | sudo -u postgres psql
+sudo -u postgres psql treetracker_migrate < treetracker.pgsql
 echo 'GRANT ALL PRIVILEGES ON DATABASE treetracker_migrate TO treetracker' | sudo -u postgres psql
-echo 'CREATE EXTENSION postgis' | sudo -u postgres psql treetracker_migrate
-psql -U treetracker -h localhost < treetracker.pgsql
 
