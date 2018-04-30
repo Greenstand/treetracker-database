cfeb5a3f3c097d3c9363ce1e579dae4ad91b687a
root <root@postgres-reinstall>
2018-04-30 21:26:37 +0000

    Set search path for postgis

--- scripts/import.pgsql.to.migrate.sh
+++ scripts/import.pgsql.to.migrate.sh
@@ -1,5 +1,9 @@
 echo 'DROP DATABASE treetracker_migrate' | sudo -u postgres psql
 echo 'CREATE DATABASE treetracker_migrate' | sudo -u postgres psql
+echo 'CREATE EXTENSION postgis' | sudo -u postgres psql treetracker_migrate
 sudo -u postgres psql treetracker_migrate < treetracker.pgsql
 echo 'GRANT ALL PRIVILEGES ON DATABASE treetracker_migrate TO treetracker' | sudo -u postgres psql
+echo 'ALTER USER postgres SET search_path TO "$user", public, postgis;' | sudo -u postgres psql
+echo 'ALTER USER treetracker SET search_path TO "$user", public, postgis;' | sudo -u postgres psql
+
 
