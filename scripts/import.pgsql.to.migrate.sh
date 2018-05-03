6ec7d220e071c3b4088948bf952c6a8c0502a917
root <root@postgres-reinstall>
2018-05-03 01:28:29 +0000

    Finishing up migrations

--- scripts/import.pgsql.to.migrate.sh
+++ scripts/import.pgsql.to.migrate.sh
@@ -3,6 +3,7 @@ echo 'CREATE DATABASE treetracker_migrate' | sudo -u postgres psql
 echo 'CREATE EXTENSION postgis' | sudo -u postgres psql treetracker_migrate
 sudo -u postgres psql treetracker_migrate < treetracker.pgsql
 echo 'GRANT ALL PRIVILEGES ON DATABASE treetracker_migrate TO treetracker' | sudo -u postgres psql
+echo 'GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO treetracker' | sudo -u postgres psql treetracker_migrate
 echo 'ALTER USER postgres SET search_path TO "$user", public, postgis;' | sudo -u postgres psql
 echo 'ALTER USER treetracker SET search_path TO "$user", public, postgis;' | sudo -u postgres psql
 
