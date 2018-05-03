8f6f66c57ef993a2dac8fd179d13a647d40e9646
Matthew Xi <matthew.fullstackgis@gmail.com>
2018-05-02 18:48:11 -0700

    Fix sequence creation

--- scripts/perform-migration-macosx.sh
+++ scripts/perform-migration-macosx.sh
@@ -3,3 +3,4 @@ source import.pgsql.to.migrate.macosx.sh
 cd ../
 db-migrate --env localhost up
 pg_dump --no-owner treetracker_migrate > treetracker_migrate.pgsql
+sed -i '' '/    AS integer/d' ./treetracker.pgsql
