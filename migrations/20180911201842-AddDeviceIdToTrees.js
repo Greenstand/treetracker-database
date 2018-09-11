72c12d78a60981949077bdb2de1c1bc1fd444f97
Matthew Xi <matthew.fullstackgis@gmail.com>
2018-09-11 13:23:14 -0700

    Added device_id to trees

---
+++ migrations/20180911201842-AddDeviceIdToTrees.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db) {
+  return db.addColumn('trees', 'device_id', 'int');
+};
+
+exports.down = function(db) {
+  return db.removeColumn('trees', 'device_id');
+};
+
+
+exports._meta = {
+  "version": 1
+};
