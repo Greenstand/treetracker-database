3aa573849d40318e8a4c9eb950ccdc2bd49d336d
Matthew Xi <matthew.fullstackgis@gmail.com>
2018-09-11 12:21:04 -0700

    Added migrations for android values in devices table and planter values in trees table

---
+++ migrations/20180911191712-AddPlanterPhotoUrlToTrees.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db) {
+  return db.addColumn('trees', 'planter_photo_url', 'string');
+};
+
+exports.down = function(db) {
+  return db.removeColumn('trees', 'planter_photo_url');
+};
+
+
+exports._meta = {
+  "version": 1
+};
