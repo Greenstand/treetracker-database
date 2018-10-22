ff02c5b5b3f9ca7e19f8b669b7b475a345146103
Matthew Xi <matthew.fullstackgis@gmail.com>
2018-10-22 16:21:49 -0700

    Added simple clusters cache table

---
+++ migrations/20181022231426-create-clusters-cache-table.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db) {
+  return db.createTable('clusters', {
+    id: { type: 'int', primaryKey: true, autoIncrement: true },
+    count: 'int'
+  });
+};
+
+exports.down = function(db) {
+  return db.dropTable('clusters');
+};
+
+exports._meta = {
+  "version": 1
+};
