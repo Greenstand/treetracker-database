d5e1a9ff41d08eb26de3e4f66b7af17357feba5e
Zaven <zaven@greenstand.org>
2020-02-16 02:45:42 +0000

    WIP load regions script

--- sql/update_region/update_regions.sh
+++ sql/update_region/update_regions.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-echo "URL for shapefile archive:"
+rm *.pgsql
+rm *.tar.gz
+
+echo "URL for shapefile archive: "
 read SHAPEFILE_URL
 
-#wget $SHAPEFILE_URL
+wget $SHAPEFILE_URL
 #[[ $SHAPEFILE_URL =~ .*/([A-z_-]+\.tar\.gz) ]]
 [[ $SHAPEFILE_URL =~ .*/(.*) ]]
 echo ${BASH_REMATCH[1]}
@@ -12,3 +15,20 @@ FILENAME=${BASH_REMATCH[1]}
 
 tar -xvzf $FILENAME
 
+[[ $FILENAME =~ (.*).tar.gz ]]
+SQLFILE=${BASH_REMATCH[1]}
+
+echo $SQLFILE
+
+echo 'SET search_path TO import, postgis, public; show search_path;' | cat - $SQLFILE > prepared_import.pgsql
+cat prepared_import.pgsql | psql postgres://treetracker@localhost/treetracker
+
+cp load_regions.sql prepared_load_regions.sql
+
+[[ $SQLFILE =~ (.*).pgsql ]]
+TABLENAME=${BASH_REMATCH[1]}
+sed -i "s/{table_name}/$TABLENAME/g" prepared_load_regions.sql
+
+echo "Region type to replace: "
+read REGION_TYPE
+sed -i "s/{region_type}/$REGION_TYPE/g" prepared_load_regions.sql
