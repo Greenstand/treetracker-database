776f12a787f0a58f036a9a73d5996601f9c904cc
Zaven <matthew.fullstackgis@gmail.com>
2020-07-19 18:10:55 -0700

    Add unique index for active_tree_region

---
+++ migrations/20200720010915-AddActiveTreeRegionUniqueIdx.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db) {
+  return db.runSql('CREATE UNIQUE INDEX active_tree_region_id_idx ON active_tree_region(id)');
+};
+
+exports.down = function(db) {
+  return db.runSql('DROP INDEX active_tree_region_id_idx');
+};
+
+exports._meta = {
+  "version": 1
+};
