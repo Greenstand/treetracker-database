216d8c43c8c493ec25717bf3d7a7ffe1745c3e25
Zaven <matthew.fullstackgis@gmail.com>
2020-08-05 13:11:39 -0700

    Add indices for zoom centers query optimization

---
+++ migrations/20200805200114-AddActiveTreeRegionZoomLevelIndex.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db) {
+  return db.runSql('CREATE INDEX active_tree_region_zoom_level_idx ON active_tree_region(zoom_level)');
+};
+
+exports.down = function(db) {
+  return db.runSql('DROP INDEX active_tree_region_zoom_level_idx');
+};
+
+exports._meta = {
+  "version": 1
+};
