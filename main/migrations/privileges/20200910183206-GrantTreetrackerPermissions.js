bdeffb54bb059eee0ecd55a840bcb6172d91890c
Zaven <matthew.fullstackgis@gmail.com>
2020-09-10 14:57:00 -0700

    Add privileges scope

---
+++ main/migrations/privileges/20200910183206-GrantTreetrackerPermissions.js
@@ -0,0 +1,102 @@
+'use strict';
+
+var async = require('async');
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db, callback) {
+  async.series(
+    [
+      db.runSql.bind(db, 'GRANT SELECT, INSERT, UPDATE ON admin_role TO treetracker'),
+      db.runSql.bind(db, 'GRANT SELECT, INSERT, UPDATE ON admin_user TO treetracker'),
+      db.runSql.bind(db, 'GRANT SELECT, INSERT, UPDATE ON admin_user_role TO treetracker'),
+      db.runSql.bind(db, 'GRANT SELECT, INSERT ON audit TO treetracker'),
+      db.runSql.bind(db, 'GRANT SELECT ON certificates TO treetracker'),
+      db.runSql.bind(db, 'GRANT SELECT ON clusters TO treetracker'),
+      db.runSql.bind(db, 'GRANT SELECT ON contract TO treetracker'),
+      db.runSql.bind(db, 'GRANT SELECT, INSERT, UPDATE ON devices TO treetracker'),
+      db.runSql.bind(db, 'GRANT SELECT ON donors TO treetracker'),
+      db.runSql.bind(db, 'GRANT SELECT ON entity TO treetracker'),
+      db.runSql.bind(db, 'GRANT SELECT ON entity_manager TO treetracker'),
+      db.runSql.bind(db, 'GRANT SELECT ON entity_relationship TO treetracker'),
+      db.runSql.bind(db, 'GRANT SELECT ON entity_role TO treetracker'),
+      db.runSql.bind(db, 'GRANT SELECT, INSERT ON note_trees TO treetracker'),
+      db.runSql.bind(db, 'GRANT SELECT, INSERT ON notes TO treetracker'),
+      db.runSql.bind(db, 'GRANT SELECT ON organizations TO treetracker'),
+      db.runSql.bind(db, 'GRANT SELECT ON payment TO treetracker'),
+      db.runSql.bind(db, 'GRANT SELECT, INSERT, UPDATE ON planter TO treetracker'),
+      db.runSql.bind(db, 'GRANT SELECT, INSERT, UPDATE ON planter_registrations TO treetracker'),
+      db.runSql.bind(db, 'GRANT SELECT, INSERT, UPDATE ON region TO treetracker'),
+      db.runSql.bind(db, 'GRANT SELECT, INSERT, UPDATE ON region_type TO treetracker'),
+      db.runSql.bind(db, 'GRANT SELECT, INSERT, UPDATE ON region_zoom TO treetracker'),
+      db.runSql.bind(db, 'GRANT SELECT ON spatial_ref_sys TO treetracker'),
+      db.runSql.bind(db, 'GRANT SELECT, INSERT, UPDATE ON tag TO treetracker'),
+      db.runSql.bind(db, 'GRANT SELECT, INSERT, UPDATE ON token TO treetracker'),
+      db.runSql.bind(db, 'GRANT SELECT, INSERT, UPDATE ON transaction TO treetracker'),
+      db.runSql.bind(db, 'GRANT SELECT, INSERT, UPDATE ON transfer TO treetracker'),
+      db.runSql.bind(db, 'GRANT SELECT, INSERT, UPDATE ON tree_attributes TO treetracker'),
+      db.runSql.bind(db, 'GRANT SELECT, INSERT, UPDATE ON tree_region TO treetracker'),
+      db.runSql.bind(db, 'GRANT SELECT, INSERT, UPDATE ON tree_species TO treetracker'),
+      db.runSql.bind(db, 'GRANT SELECT, INSERT, UPDATE ON tree_tag TO treetracker'),
+      db.runSql.bind(db, 'GRANT SELECT, INSERT, UPDATE ON trees TO treetracker')
+    ],
+    callback
+  );
+};
+
+exports.down = function(db, callback) {
+  async.series(
+    [
+      db.runSql.bind(db, 'REVOKE ALL PRIVILEGES ON admin_role FROM treetracker'),
+      db.runSql.bind(db, 'REVOKE ALL PRIVILEGES ON admin_user FROM treetracker'),
+      db.runSql.bind(db, 'REVOKE ALL PRIVILEGES ON admin_user_role FROM treetracker'),
+      db.runSql.bind(db, 'REVOKE ALL PRIVILEGES ON audit FROM treetracker'),
+      db.runSql.bind(db, 'REVOKE ALL PRIVILEGES ON certificates FROM treetracker'),
+      db.runSql.bind(db, 'REVOKE ALL PRIVILEGES ON clusters FROM treetracker'),
+      db.runSql.bind(db, 'REVOKE ALL PRIVILEGES ON contract FROM treetracker'),
+      db.runSql.bind(db, 'REVOKE ALL PRIVILEGES ON devices FROM treetracker'),
+      db.runSql.bind(db, 'REVOKE ALL PRIVILEGES ON donors FROM treetracker'),
+      db.runSql.bind(db, 'REVOKE ALL PRIVILEGES ON entity FROM treetracker'),
+      db.runSql.bind(db, 'REVOKE ALL PRIVILEGES ON entity_manager FROM treetracker'),
+      db.runSql.bind(db, 'REVOKE ALL PRIVILEGES ON entity_relationship FROM treetracker'),
+      db.runSql.bind(db, 'REVOKE ALL PRIVILEGES ON entity_role FROM treetracker'),
+      db.runSql.bind(db, 'REVOKE ALL PRIVILEGES ON note_trees FROM treetracker'),
+      db.runSql.bind(db, 'REVOKE ALL PRIVILEGES ON notes FROM treetracker'),
+      db.runSql.bind(db, 'REVOKE ALL PRIVILEGES ON organizations FROM treetracker'),
+      db.runSql.bind(db, 'REVOKE ALL PRIVILEGES ON payment FROM treetracker'),
+      db.runSql.bind(db, 'REVOKE ALL PRIVILEGES ON planter FROM treetracker'),
+      db.runSql.bind(db, 'REVOKE ALL PRIVILEGES ON planter_registrations FROM treetracker'),
+      db.runSql.bind(db, 'REVOKE ALL PRIVILEGES ON region FROM treetracker'),
+      db.runSql.bind(db, 'REVOKE ALL PRIVILEGES ON region_type FROM treetracker'),
+      db.runSql.bind(db, 'REVOKE ALL PRIVILEGES ON region_zoom FROM treetracker'),
+      db.runSql.bind(db, 'REVOKE ALL PRIVILEGES ON spatial_ref_sys FROM treetracker'),
+      db.runSql.bind(db, 'REVOKE ALL PRIVILEGES ON tag FROM treetracker'),
+      db.runSql.bind(db, 'REVOKE ALL PRIVILEGES ON token FROM treetracker'),
+      db.runSql.bind(db, 'REVOKE ALL PRIVILEGES ON transaction FROM treetracker'),
+      db.runSql.bind(db, 'REVOKE ALL PRIVILEGES ON transfer FROM treetracker'),
+      db.runSql.bind(db, 'REVOKE ALL PRIVILEGES ON tree_attributes FROM treetracker'),
+      db.runSql.bind(db, 'REVOKE ALL PRIVILEGES ON tree_region FROM treetracker'),
+      db.runSql.bind(db, 'REVOKE ALL PRIVILEGES ON tree_species FROM treetracker'),
+      db.runSql.bind(db, 'REVOKE ALL PRIVILEGES ON tree_tag FROM treetracker'),
+      db.runSql.bind(db, 'REVOKE ALL PRIVILEGES ON trees FROM treetracker')
+
+    ],
+    callback
+  );
+};
+
+exports._meta = {
+  "version": 1
+};
