1e558f6bbf95424757309419296041d7237cd54a
uchan3 <urey.chan3@gmail.com>
2020-11-29 09:37:34 -0700

    Fix planter script

--- etl/planterFK.py
+++ etl/planterFK.py
@@ -3,8 +3,6 @@ import psycopg2 as db
 from decouple import config
 from faker import Faker
 
-from CustomTransformations import entity
-
 #ETL Script for 'public.planter' table. 
 
 #Extract
@@ -18,7 +16,7 @@ ODBPORT = config('ORIGINAL_DBPORT')
 oConnection = db.connect(database=ODBNAME, user=ODBUSER, password=ODBPASSWORD, 
 host = ODBHOST, port = ODBPORT)
 
-#Since we use target entity table to fill values in planter table, need target DB. 
+#Since we use target entity table to fill fields in planter table, need target DB. 
 #Define database connection paraemters for target database
 TDBNAME = config('TARGET_DBNAME')
 TDBUSER = config('TARGET_DBUSER')
@@ -35,56 +33,73 @@ WHERE person_id is not NULL OR organization_id is not NULL""")
 entityTable = etl.fromdb(tConnection, 'SELECT * FROM public.entity')
 
 #Transform
+
 #Select fields from entityTable
-entityTable = etl.cut(entityTable, 'id', 'name', 'first_name', 'last_name')
+entityTable = etl.cut(entityTable, 'id', 'name', 'first_name', 'last_name', 'email', 'phone')
 
 #Rename fields in entityTable
-entityTable = etl.rename(entityTable, {'first_name': 'f_name', 'last_name': 'l_name'})
+entityTable = etl.rename(entityTable, {
+    'first_name': 'f_name', 
+    'last_name': 'l_name', 
+    'email': 'email_address', 
+    'phone': 'phone_number'})
 
-#Join planterTable with entityTable on organization_id
-joinTable = etl.leftjoin(table, entityTable, lkey='organization_id', rkey='id')
-
-# #Split table based on whether name field is null or not. 
-joinTableNull = etl.select(joinTable, 'name', lambda row: row == None)
-joinTableNotNull = etl.select(joinTable, 'name', lambda row: row != None)
-
-# #Convert values in organization field based on values in name field. 
-joinTableNull = etl.convert(joinTableNull, 'organization', lambda org, row: row.name, pass_row=True)
-joinTableNotNull = etl.convert(joinTableNotNull, 'organization', lambda org, row: row.name, pass_row=True)
-
-# #Combine tables. 
-joinTable = etl.cat(joinTableNull, joinTableNotNull)
-
-# #Remove values
-joinTable = etl.cutout(joinTable, 'name', 'f_name', 'l_name')
-
-# #Join planter Table on entity Table on person_id
-joinTable = etl.leftjoin(joinTable, entityTable, lkey='person_id', rkey='id')
-
-# # #Split table based on whether name field is null or not. 
-joinTableNotNull = etl.select(joinTable, 'f_name', lambda row: row != None)
-joinTableNull = etl.select(joinTable, 'f_name', lambda row: row == None)
-
-# # #Convert values
+#Create instance of faker class
 fake = Faker(['en_US', 'en_GB']) #Locales: United States/Great Britain
 Faker.seed(1234)
 
-joinTableNotNull = etl.convert(joinTable, {
-    'first_name': lambda f_name, row: row.f_name, 
-    'last_name': lambda l_name, row: row.l_name}, pass_row=True)
+#Left join planterTable with entityTable on organization_id
+joinTable = etl.leftjoin(table, entityTable, lkey='organization_id', rkey='id')
 
+#Split table based on whether name field is null or not. 
+joinTableNull = etl.select(joinTable, 'name', lambda row: row == None)
+joinTableNotNull = etl.select(joinTable, 'name', lambda row: row != None)
+
+#Convert values in 'organization' field based on values in 'name' field. 
+joinTableNull = etl.convert(joinTableNull, 'organization', lambda org, row: row.name, pass_row=True)
+joinTableNotNull = etl.convert(joinTableNotNull, 'organization', lambda org, row: row.name, pass_row=True)
+
+#Combine tables. 
+joinTable = etl.cat(joinTableNull, joinTableNotNull)
+
+#Remove fields. 
+joinTable = etl.cutout(joinTable, 'name', 'f_name', 'l_name', 'email_address', 'phone_number')
+
+#Left join planter Table on entity Table on person_id
+joinTable = etl.leftjoin(joinTable, entityTable, lkey='person_id', rkey='id')
+
+#Split table based on whether 'first name' field is null or not. 
+joinTableNotNull = etl.select(joinTable, 'f_name', lambda row: row != None)
+joinTableNull = etl.select(joinTable, 'f_name', lambda row: row == None)
+
+#Convert values
+#Here, if 'f_name' is not null, we have a planter entity. 
+#So, we use values from planter entity to fill in fields.
+joinTableNotNull = etl.convert(joinTableNotNull, {
+    'first_name': lambda f_name, row: row.f_name, 
+    'last_name': lambda l_name, row: row.l_name, 
+    'email': lambda email, row: row.email_address, 
+    'phone': lambda phone, row: row.phone_number}, pass_row=True)
+
+#Else, we populate fields with random values. 
 joinTableNull = etl.convert(joinTableNull, {
     'first_name': lambda f_name: fake.first_name(), 
-    'last_name': lambda l_name: fake.last_name()
-})
+    'last_name': lambda l_name: fake.last_name(), 
+    'email': lambda email: fake.email(), 
+    'phone': lambda phone: fake.phone_number()})
 
+#Combine tables
 joinTable = etl.cat(joinTableNotNull, joinTableNull)
 
-# #Remove values
-joinTable = etl.cutout(joinTable, 'name', 'f_name', 'l_name')
+#Remove fields
+joinTable = etl.cutout(joinTable,'name', 'f_name', 'l_name', 'email_address', 'phone_number')
+
+#Anonymize image_url
+joinTable = etl.convert(joinTable, 'image_url', lambda row: fake.image_url())
+
+#Select unique rows in table based on email criteria
+joinTable = etl.distinct(joinTable, 'email')
+etl.tocsv(joinTable, 'planter.csv')
 
 #Load rows into database table. 
 etl.todb(joinTable, tConnection, 'planter', 'public')
-
-# for row in joinTableNotNull[1:20]:
-#     print(row)
